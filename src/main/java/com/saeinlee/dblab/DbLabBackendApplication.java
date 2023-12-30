c35ed862e7ac13f21cf55fa6260e0cffaf1c69f2
saeinlee <gitsaein@gmail.com>
2023-12-30 12:03:17 +0900

    init

---
+++ src/main/java/com/saeinlee/dblab/DbLabBackendApplication.java
@@ -0,0 +1,13 @@
+package com.saeinlee.dblab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbLabBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbLabBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saeinlee/dblab/DbLabBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saeinlee.dblab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbLabBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
